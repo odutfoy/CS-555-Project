41c023d2605fa431a2924c323c3b99de6c7339bb
Lassaux Nicolas <nico401.lassaux@hotmail.fr>
2015-12-12 17:03:45 -0600

    Add comments struc.h

--- src/struct.h
+++ src/struct.h
@@ -1,13 +1,22 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
+/**
+  clock: the simulation clock
+  server_status: the number of busy servers
+  customers_in_queue: the number of customers in the queue
+  time_of_last_event: the begining time of the next event
+  number_blocked: overall number of blocked customers
+  total_number_customer: overall number of customers
+  total_time_spent: overall time spent in the system by customers
+**/
 struct Parameters{
   int clock;
   int server_status;
   int customers_in_queue;
   int time_of_last_event;
   int number_blocked;
-  int total_customer_num;
+  int total_number_customer;
   int total_time_spent;
 };
 
